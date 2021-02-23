a955e377485357ddfbefc01703ade4640e744535
duongdam <duong.dam@classfunc.com>
2021-02-23 16:39:06 +0700

    Updates

---
+++ precache-manifest.8752eef7e2363942990bf10ff5c19526.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "886aa35a46abf3b8ec65ac85a0c9fb24",
+    "url": "./index.html"
+  },
+  {
+    "revision": "99427242cf91f0bc2716",
+    "url": "./static/css/2.4a00cb5b.chunk.css"
+  },
+  {
+    "revision": "abcfeb1f91ec361de504",
+    "url": "./static/css/main.2d9e6042.chunk.css"
+  },
+  {
+    "revision": "99427242cf91f0bc2716",
+    "url": "./static/js/2.1e83d62e.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "./static/js/2.1e83d62e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abcfeb1f91ec361de504",
+    "url": "./static/js/main.e4e35ce0.chunk.js"
+  },
+  {
+    "revision": "eff7ae4530773ec5d2ba",
+    "url": "./static/js/runtime-main.b5fb1376.js"
+  },
+  {
+    "revision": "8f2e8843f06a3382810b545ef78284be",
+    "url": "./static/media/font.8f2e8843.svg"
+  },
+  {
+    "revision": "2898d4d9bf2c85076b21b8ede20fa190",
+    "url": "./static/media/hubman.2898d4d9.svg"
+  },
+  {
+    "revision": "5cc36831bf9ce180e4fc56d1c1d2c14e",
+    "url": "./static/media/index.5cc36831.less"
+  },
+  {
+    "revision": "c17ec543e0d6a592928535f43867f969",
+    "url": "./static/media/index.c17ec543.less"
+  },
+  {
+    "revision": "c25862507db4cd9798194ad4f92b2a92",
+    "url": "./static/media/numMonitor.c2586250.svg"
+  }
+]);
